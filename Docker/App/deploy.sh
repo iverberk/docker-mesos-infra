20ce6e83615ba743e9fa7ba6b27b3282e4e8f1fa
Ivo Verberk <verberk@ict.eur.nl>
2015-04-03 18:48:51 +0200

    Fix deploy specification and script

--- Docker/App/deploy.sh
+++ Docker/App/deploy.sh
@@ -4,15 +4,16 @@ set -e
 
 marathon="192.168.33.11"
 
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app-$APP_ENV &> /dev/null
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app-$APP_ENV
 
 sleep 1
 
 sed -i'.orig' "s/{{APP_VERSION}}/${APP_VERSION}/g" app.json
-sed -i'.orig' "s/{{APP_ENV}}/${APP_ENV}/g" app.json
+sed -i'.orig2' "s/{{APP_ENV}}/${APP_ENV}/g" app.json
 
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps?force=true -d@app.json &> /dev/null
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app.json
 
 mv -f app.json.orig app.json
+rm -f app.json.orig2
 
 exit 0
